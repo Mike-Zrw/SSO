ff63d9469d9d70b00ccfc875ba7ea8a23bd49fc7
mike.zhang <1836468472@qq.com>
2018-06-27 10:12:49 +0800

    优化提示

---
+++ AbsUserCenter.SSOClient/BPermission.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbsUserCenter.SSOClient
+{
+    public partial class BPermission
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int PMS_ID { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string PMS_DESC { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Nullable<int> PMS_PMT_ID { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int PMS_PARENT_ID { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Nullable<int> PMS_SYS_ID { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool PMS_DELETED_FLAG { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int PMS_ORDER_NUM { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string PMS_CREATIONUID { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime PMS_CREATION_DT { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string PMS_URL { get; set; }
+
+    }
+}
